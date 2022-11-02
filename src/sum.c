49383bf30280899fb2f2235f49c736ca9b7af4c2
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2022-11-02 08:50:40 +0100

    opg 2 done

--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,15 @@
 #include "sum.h"
+#include <assert.h>
 
 int sum(int a[], int n)
 {
-    return 0;
+    assert(n >= 0); /*Precondition*/
+    if (n == 0)     /*Base case*/
+    {
+        return 0;
+    }
+    else /*Recursive step*/
+    {
+        return a[n - 1] + sum(a, n - 1);
+    }
 }
