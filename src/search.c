6b0f1bd5e91dbd3339a62265d145a4ed8ef9f8f5
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2022-11-02 09:27:13 +0100

    search done

--- src/search.c
+++ src/search.c
@@ -1,7 +1,20 @@
 #include "search.h"
 #include <stdbool.h>
+#include <assert.h>
 
 bool search(int a[], int n, int x)
 {
-    return 0;
+    assert(n >= 0);             /*precondition*/
+    if (n > 0 && a[n - 1] == x) /*base case*/
+    {
+        return true;
+    }
+    else if (n > 0 && a[n - 1] != x) /*recursive step*/
+    {
+        return search(a, n - 1, x);
+    }
+    else /*base case (n == 0)*/
+    {
+        return false;
+    }
 }
